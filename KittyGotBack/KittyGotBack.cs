92f6d91b068a61d10d73b8b2d5f483d473526994
Mike Ruhl <Mike.Ruhl@tobiidynavox.com>
2018-05-21 19:50:16 -0400

    finished

--- KittyGotBack/KittyGotBack.cs
+++ KittyGotBack/KittyGotBack.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Fabric;
+using System.Fabric.Query;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading;
 using System.Threading.Tasks;
 using KittyCore;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Remoting.Runtime;
-using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using StatefulService = Microsoft.ServiceFabric.Services.Runtime.StatefulService;
 
 namespace KittyGotBack
 {
@@ -16,6 +21,8 @@ namespace KittyGotBack
     /// </summary>
     internal sealed class KittyGotBack : StatefulService, IKittyBackendService
     {
+        private CloudBlobContainer _blobContainer;
+
         public KittyGotBack(StatefulServiceContext context)
             : base(context)
         { }
@@ -32,6 +39,27 @@ namespace KittyGotBack
             return this.CreateServiceRemotingReplicaListeners();
         }
 
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            var storageConnectionString = Environment.GetEnvironmentVariable("containerConnectionString");
+
+            if (CloudStorageAccount.TryParse(storageConnectionString, out var storageAccount))
+            {
+                ServiceEventSource.Current.ServiceMessage(Context, "Connected to storage account");
+                var operationContext = new OperationContext();
+                var client = storageAccount.CreateCloudBlobClient();
+                _blobContainer = client.GetContainerReference("blobberino");
+                await _blobContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Off,
+                    new BlobRequestOptions(),
+                    operationContext, cancellationToken);
+                return;
+            }
+
+            ServiceEventSource.Current.ServiceMessage(Context, "Could not connect to storage account.  Failure.");
+            throw new ArgumentException("Connection String specified is invalid");
+
+        }
+
         /// <summary>
         /// This is the main entry point for your service replica.
         /// This method executes when this replica of your service becomes primary and has write status.
@@ -39,7 +67,7 @@ namespace KittyGotBack
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
         public async Task<byte[]> GetImageAsync(int width, int height)
         {
-            var kitties = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, byte[]>>("kitties");
+            var kitties = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, string>>("kitties");
 
             var requestKey = $"{width}x{height}";
 
@@ -48,7 +76,14 @@ namespace KittyGotBack
                 var result = await kitties.TryGetValueAsync(tx, requestKey);
 
                 if (result.HasValue)
-                    return result.Value;
+                {
+                    var blobRef = _blobContainer.GetBlockBlobReference(result.Value);
+                    await blobRef.FetchAttributesAsync();
+                    var imageArray = new byte[blobRef.Properties.Length];
+                    await blobRef.DownloadToByteArrayAsync(imageArray, 0);
+                    ServiceEventSource.Current.ServiceMessage(Context, $"Found image in Azure cache, returning: {requestKey}.");
+                    return imageArray;
+                }
 
 
                 var client = new HttpClient();
@@ -61,10 +96,22 @@ namespace KittyGotBack
 
                 var bytes = await get.Content.ReadAsByteArrayAsync();
 
-                await kitties.AddOrUpdateAsync(tx, requestKey, bytes, (key, value) => bytes);
+                var newKey = string.Empty;
 
+                using (var sha1 = new SHA1CryptoServiceProvider())
+                {
+                    newKey = Convert.ToBase64String(sha1.ComputeHash(bytes));
+                }
+
+                var newBlob = _blobContainer.GetBlockBlobReference(newKey);
+
+                var addToDictionary = kitties.AddOrUpdateAsync(tx, requestKey, newKey, (key, value) => newKey);
+                var addToAzure = newBlob.DeleteIfExistsAsync().ContinueWith(t=> newBlob.UploadFromByteArrayAsync(bytes, 0, bytes.Length));
+
+                await Task.WhenAll(addToAzure, addToDictionary);
+                
                 await tx.CommitAsync();
-
+                ServiceEventSource.Current.ServiceMessage(Context, $"Had to download image with key {requestKey}, now it lives in Azure under the hash: {newKey}.");
                 return bytes;
             }
 
